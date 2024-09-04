46d8b91d0afdf43fee8138928fb0742de7589fe2
Marcelo <marcelorcarnauba@gmail.com>
2024-09-04 17:17:54 -0300

    feat(client): Confirmar senha no cadastro de usuário

--- client/src/components/cadastro/index.tsx
+++ client/src/components/cadastro/index.tsx
@@ -12,8 +12,16 @@ const CadastroClient: React.FC = ({ }) => {
         nome: "",
         sobrenome: "",
         email: "",
-        senha: ""
+        senha: "",
+        confirmarSenha: ""
     })
+
+    const validarSenha = () => {
+        if(cadastro.senha != cadastro.confirmarSenha) {
+            return false
+        }
+        return true
+    }
 
     const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
@@ -23,7 +31,13 @@ const CadastroClient: React.FC = ({ }) => {
     }
     const HandleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const result = cadastrarUserSchema.safeParse(cadastro)
+        const validar = validarSenha()
+
+        if(!validar){
+            return toast.error("A senha no campo Confirmar Senha deve ser igual a Senha")
+        }
+
+        const result = cadastrarUserSchema.safeParse(cadastro);
 
         if (!result.success) {
             result.error.issues.forEach((err) => {
@@ -39,9 +53,10 @@ const CadastroClient: React.FC = ({ }) => {
                 }
             })
             if (!res.ok) {
-                return console.log({ message: "Não foi possivel cadastrar o usuário", result: res })
+                //return console.log({ message: "Não foi possivel cadastrar o usuário", result: res })
+                return toast.error("Não foi possível cadastrar o usuário")
             }
-            console.log({ message: "Usuário cadastrado com sucesso", result: res })
+            //console.log({ message: "Usuário cadastrado com sucesso", result: res })
             return toast.success("Usuário cadastrado com sucesso")
         }
         catch (err) {
@@ -51,20 +66,22 @@ const CadastroClient: React.FC = ({ }) => {
     }
 
     return (
-        <main className='flex flex-col w-full h-screen justify-center items-center gap-10'>
-            <section className='flex flex-col text-center'>
+        <main className='flex flex-col w-full h-screen items-center gap-10 justify-between'>
+            <section className='flex flex-col text-center pt-16'>
                 <h1 className='text-2xl font-semibold'>Bem-Vindo!</h1>
                 <h2 className='text-sm'>Crie sua conta agora</h2>
             </section>
+
             <form onSubmit={HandleSubmit} className='flex flex-col w-full gap-4 items-center px-10'>
                 <input onChange={HandleChange} type="text" name="nome" id="nome" placeholder='Nome' />
                 <input onChange={HandleChange} type="text" name="sobrenome" id="sobrenome" placeholder='Sobrenome' />
                 <input onChange={HandleChange} type="email" name="email" id="email" placeholder='E-mail' />
                 <input onChange={HandleChange} type="password" name="senha" id="senha" placeholder='Senha' />
-                <button type='submit'>Cadastrar-se</button>
+                <input onChange={HandleChange} type="password" name="confirmarSenha" id="confirmarSenha" placeholder='Confirmar Senha' />
+                <button className='border w-full rounded-xl py-2' type='submit'>Cadastrar-se</button>
             </form>
 
-            <div>
+            <div className='py-6'>
                 Ja possui uma conta? <Link href={"/"} >Fazer Login</Link>
             </div>
         </main>
--- client/src/schemas/userSchemas.ts
+++ client/src/schemas/userSchemas.ts
@@ -5,6 +5,7 @@ export const cadastrarUserSchema = z.object({
     sobrenome: z.string().min(3, "O sobrenome deve conter pelo menos 3 caracteres").max(120, "O sobrenome deve conter no máximo 120 caracteres"),
     email: z.string().email("Email inválido"),
     senha: z.string().min(6, "A senha deve conter pelo menos 6 caracteres").max(30, "A senha deve conter no máximo 30 caracteres"),
+    confirmarSenha: z.string().nullable()
 })
 
 export type cadastrarUserType = z.infer<typeof cadastrarUserSchema>
